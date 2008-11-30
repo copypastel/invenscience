6f3b0fa3d18cc7c9fad802f3da8352f4e8474736
ecin <ecin@copypastel.com>
2008-11-30 07:33:39 -0400

    Changed Detail's price attribute to be stored as a BigDecimal instead of Integer, to avoid having conversions for currency between the database and the presentation.

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -2,23 +2,11 @@ class Detail
   include DataMapper::Resource
   
   property :id, Serial
-  property :price, Integer
+  property :price, BigDecimal
   property :quantity, Integer
 
   belongs_to :order
   belongs_to :item
   has 1, :warehouse, :through => :item
-
-=begin
-  def price=(new_price)
-    attribute_set(:price, (new_price * 100).to_i)
-  end
-  
-  def price
-    p = attribute_get(:price)
-    return nil if p.nil?
-    attribute_get(:price) / 100.0
-  end
-=end
   
 end
