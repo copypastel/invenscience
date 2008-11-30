fef2969623d5ae4a4e70bbaf9484add5aceaf92d
ecin <ecin@copypastel.com>
2008-11-30 06:52:47 -0400

    Added Warehouse relation to Order, plus the awesome add method which takes an item and quantity, and creates a Detail for the Order in question.

--- app/models/order.rb
+++ app/models/order.rb
@@ -10,10 +10,25 @@ class Order
 
   has n, :details
   has n, :items, :through => :details
+  has n, :warehouses, :through => :items
 
   # Place the order
   # Needs to go organize the details according to warehouse, and place an order for each
   def place
+  end
+  
+  def add(item, quantity)
+    return nil if quantity <= 0
+    if items.include? item
+      detail = details.first(:item_id => item.id)
+      detail.quantity += quantity
+      detail.price = item.price(detail.quantity) * 100
+      detail.save
+      return detail
+    else
+      price = item.price(quantity) * 100
+      return Detail.create(:item => item, :quantity => quantity, :price => price, :order => self)
+    end
   end
   
   # Return total for the order
