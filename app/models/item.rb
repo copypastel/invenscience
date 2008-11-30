c22554b7623409bb9743bfb598bed26943a46f4c
ecin <ecin@copypastel.com>
2008-11-30 06:51:22 -0400

    Added uri and description properties to Item. Also added a relation to Warehouse, and a price method to determine the price of the item depending on quantity.

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,13 +3,20 @@ class Item
   
   property :id, Serial
   property :name, String, :nullable => false
+  property :description, Text
+  property :uri, Text, :nullable => false
   
   has n, :details
   has n, :orders, :through => :details
-
+  belongs_to :warehouse
+  
   # Check if this item is avaiable from the warehouse provided
   def available?( warehouse )
     
+  end
+
+  def price(quantity = nil)
+    warehouse.price(self, quantity)
   end
 
 end
