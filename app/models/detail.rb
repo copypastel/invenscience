27b14e5b020137b36b09b55d646099389646073f
ecin <ecin@copypastel.com>
2008-11-30 06:48:11 -0400

    Added Warehouse relation to Detail. A detail can now access the warehouse its item belongs to.

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -5,8 +5,20 @@ class Detail
   property :price, Integer
   property :quantity, Integer
 
-  belongs_to :warehouse
   belongs_to :order
   belongs_to :item
+  has 1, :warehouse, :through => :item
 
+=begin
+  def price=(new_price)
+    attribute_set(:price, (new_price * 100).to_i)
+  end
+  
+  def price
+    p = attribute_get(:price)
+    return nil if p.nil?
+    attribute_get(:price) / 100.0
+  end
+=end
+  
 end
