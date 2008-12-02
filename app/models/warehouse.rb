17a31a84b5e37932091a72027efa6bbcb13868d6
Mathew Wilson <daicoden@copypastel.com>
2008-12-02 06:30:35 -0500

    Updated the way that parsers were being mixed into the warehouse.
    The new way of mixing in a parser fixes the bug which occured when overwriting an existing parser.  The model must be saved first to get this fix which will be changed in the next fix.  Parsers are now set anytime the model is read from cash or the database.
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    #
    # Committer: Mathew Wilson <daicoden@copypastel.com>
    #
    # On branch refactoring_and_testing_warehouses
    # Changes to be committed:
    #   (use "git reset HEAD <file>..." to unstage)
    #
    #	modified:   app/models/warehouse.rb
    #

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,5 +1,42 @@
-class Warehouse
+# You can integrate this into Warehouse if this is too complicated
+# but it provides nice seperation of code
+# TODO: Figure out how to move this after warehouse
+module ParserManager
+  def self.included(warehouse)
+    warehouse.before :save, :check_parser
+    warehouse.after  :save, :cleanup_parser
+    warehouse.extend ClassMethods
+  end
+  
+  @dirty_parser = false
+  
+  # Setup code for cleanup_parser
+  def check_parser()
+    @dirty_parser = self.attribute_dirty?(:parser)
+  end
+  
+  # Handles when a parser is changed
+  def cleanup_parser() 
+    if @dirty_parser and not self.parser.nil?
+      # *Need to figure out how to unextend previous parser!
+      # *Luckily the method used is the latest module extended 
+      extend self.parser
+    end
+  end
+
+  module ClassMethods
+    # Handles when an object is retrieved from the database
+    def load(values, query)
+      warehouse = super(values,query)
+      warehouse.extend warehouse.parser unless warehouse.parser.nil?
+      warehouse
+    end
+  end
+end
+
+class Warehouse  
   include DataMapper::Resource
+  include ::ParserManager
   
   property :id, Serial
   property :name, String, :nullable => false, :unique => true
@@ -9,20 +46,7 @@ class Warehouse
   has n, :details, :through => :items
   has n, :orders, :through => :details
   has n, :carts
-
-  
-  def method_missing(method_name, *args)
-
-    extend parser unless parser.nil?
-
-    if self.respond_to?(method_name)
-      self.send( method_name, *args)
-    else
-      super
-    end
     
-  end
-
   # Receive a url for an item.
   # Return an array with all items that were parsed from that url.
   def self.parse(uri)
@@ -43,3 +67,4 @@ class Warehouse
   end
 
 end
+
