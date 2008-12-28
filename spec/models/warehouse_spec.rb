e327d7ae0436c3e01652f4da80490e7a782dbb97
daicoden <daicoden@copypastel.com>
2008-12-28 12:50:28 -0700

    Verry small update in an attempt to get the ball rolling again.

--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -1,67 +1,17 @@
 require File.join( File.dirname(__FILE__), '..', "spec_helper" )
 require File.dirname(__FILE__) + "/../parsers/shared_parser_examples"
 require File.join( File.dirname(__FILE__),'factories','warehouse_factory' )
-
-<<<<<<< HEAD:spec/models/warehouse_spec.rb
-module WarehouseSpecHelper
-  # See parser specs for parser functionality!
-  module ValidParser1
-    def parse(uri)
-      ValidParser1.responce
-    end
-    
-    def self.responce
-      "responce 1"
-    end
-  end
-  
-  module ValidParser2
-    def parse(uri)
-      ValidParser2.responce
-    end
-    
-    def self.responce
-      "responce 2"
-    end
-  end
-  
-  #testing for the uri should go in parser spec
-  def good_uri
-    "Good URI" #Could switch this to http:// and have a bad_uri for testing
-  end
-  
-  def valid_warehouse
-    warehouse = Warehouse.new
-    warehouse.name = "Valid Name"
-    warehouse.parser = ValidParser1
-    warehouse
-  end
-  
-  def saved_warehouse
-    #switch to fixtures!
-    warehouse = Warehouse.first 
-    if(warehouse.nil?)
-      warehouse = valid_warehouse
-      warehouse.name = "Original Valid Name"
-      warehouse.save
-    end
-    warehouse
-  end
-end
-
-shared_examples_for "a new record" do
-=======
+ 
 #-----------------------------------------#
 #           Shared Examples               #
 #-----------------------------------------#
 shared_examples_for "a new record" do     
->>>>>>> master:spec/models/warehouse_spec.rb
   it "should be a new record" do
     @warehouse.should be_new_record
   end
 end
-
-
+ 
+ 
 #-----------------------------------------#
 #            Warehouse Spec               #
 #-----------------------------------------#
@@ -103,22 +53,13 @@ describe Warehouse do
       
   #  end
 end
-
-
-<<<<<<< HEAD:spec/models/warehouse_spec.rb
-describe Warehouse do
-  include WarehouseSpecHelper
-  
-  describe "when newly created" do
-    before(:each) do
-      @warehouse = Warehouse.new
-=======
+ 
+ 
 =begin
   describe "when newly created" do
     
     before(:each) do
       @warehouse = Factory::Warehouse[:new]
->>>>>>> master:spec/models/warehouse_spec.rb
     end
     
 #    it_should_have_property(:name)   with(:unique => true)
@@ -140,18 +81,6 @@ describe Warehouse do
       @warehouse.parser.should  be(nil)
       @warehouse.id.should      be(nil)
     end
-<<<<<<< HEAD:spec/models/warehouse_spec.rb
-       
-    it "should not be valid without a name" do
-      Kernel.system("growlnotify -m " + "HELLO")
-      #@warehouse.errors_on(:name).should be_valid
-    end
-    
-    it "should not be valid without parser()" do
-      @warehouse.parser = nil
-      
-      @warehouse.should_not be_valid
-=======
   end
   
   describe "when not saved" do
@@ -169,7 +98,6 @@ describe Warehouse do
     it "should not be valid without #parser" do
       @warehouse.parser = nil
       @warehouse.should_not be(:valid)
->>>>>>> master:spec/models/warehouse_spec.rb
     end
   end
   
@@ -177,17 +105,17 @@ describe Warehouse do
     before(:each) do
       @warehouse = Factory::Warehouse[:valid]
     end
-
+ 
     it "should be valid" do
       @warehouse.should be(:valid)
     end
-
+ 
     it "should have an original name" do
       @warehouse.name = Factory::Warehouse[:saved].name
       @warehouse.should_not be(:valid)
     end
   end
-
+ 
   describe "after save" do
     before(:each) do
       @warehouse = Factory::Warehouse[:new]
@@ -197,5 +125,5 @@ describe Warehouse do
       @warehouse.should_not be(:valid)
     end
   end
-
+ 
 =end
